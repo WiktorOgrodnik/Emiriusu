10559856020d0fc72c03619285d80172af1bda2a
Mikołaj Hasik <mikoajhasik@gmail.com>
2019-08-22 14:16:40 +0200

    rozwinięcie buttona

--- SFMLSample/Button.cpp
+++ SFMLSample/Button.cpp
@@ -2,11 +2,35 @@
 #include "Button.h"
 
 
-Button::Button()
+Button::Button(float x, float y, float width, float height, sf::RectangleShape shape, sf::Font font, sf::Text text, sf::Color idlecolor, sf::Color activecolor)
 {
+	shape.setPosition(sf::Vector2f(x, y));
+	shape.setSize(sf::Vector2f(width, height));
+	shape.setFillColor(sf::Color(idlecolor));
+	//to do tekstu w guzikach
+	//text.setCharacterSize(14);
+	//text.setString(text);
+	//text.setPosition();
+	//text.setFillColor(sf::Color::White);
 }
-
 
 Button::~Button()
 {
+}
+
+Function* Button::onSelect()
+{
+	shape.setFillColor(sf::Color(activecolor));
+	return nullptr;
+}
+
+Function* Button::onClick()
+{
+	return nullptr;
+}
+
+Function* Button::onDeselect()
+{
+	shape.setFillColor(sf::Color(idlecolor));
+	return nullptr;
 }
--- SFMLSample/Button.h
+++ SFMLSample/Button.h
@@ -1,13 +1,21 @@
 #pragma once
 #include "pch.h" 
-class Button
+#include "Selectable.h"
+class Button : public Selectable
 {
 	Function* activationFunction;
+	sf::RectangleShape shape;
+	sf::Font font;
+	sf::Text text;
+	sf::Color idlecolor;
+	sf::Color activecolor;
 
 public:
-	Button();
+	Button(float x, float y, float width, float height, sf::RectangleShape shape, sf::Font font, sf::Text text, sf::Color idlecolor, sf::Color activecolor);
 	~Button();
 	void setActivationFunction(Function* newActivationFunction) { activationFunction = newActivationFunction; }
 	Function* getActivationFunction() { return activationFunction; }
+	virtual Function* onSelect()override;
+	virtual Function* onClick()override;
+	virtual Function* onDeselect()override;
 };
-
